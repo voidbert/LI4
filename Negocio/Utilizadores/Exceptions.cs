c039789fcdf97231985142bca9c3d7deeaf95f8f
voidbert <humbertogilgomes@protonmail.com>
2025-01-12 03:09:21 +0000

    Add some user operations to business layer

---
+++ Negocio/Utilizadores/Exceptions.cs
@@ -0,0 +1,41 @@
+namespace LI4.Negocio.Utilizadores;
+
+[Serializable]
+public class UtilizadorNaoEncontradoException : Exception
+{
+    public UtilizadorNaoEncontradoException() { }
+    public UtilizadorNaoEncontradoException(string message) : base(message) { }
+    public UtilizadorNaoEncontradoException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+[Serializable]
+public class PalavraPasseIncorretaException : Exception
+{
+    public PalavraPasseIncorretaException() { }
+    public PalavraPasseIncorretaException(string message) : base(message) { }
+    public PalavraPasseIncorretaException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+[Serializable]
+public class ImpedidoDeIniciarSessaoException : Exception
+{
+    public ImpedidoDeIniciarSessaoException() { }
+    public ImpedidoDeIniciarSessaoException(string message) : base(message) { }
+    public ImpedidoDeIniciarSessaoException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+[Serializable]
+public class EnderecoEletronicoInvalidoException : Exception
+{
+    public EnderecoEletronicoInvalidoException() { }
+    public EnderecoEletronicoInvalidoException(string message) : base(message) { }
+    public EnderecoEletronicoInvalidoException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+[Serializable]
+public class EnderecoEletronicoExistenteException : Exception
+{
+    public EnderecoEletronicoExistenteException() { }
+    public EnderecoEletronicoExistenteException(string message) : base(message) { }
+    public EnderecoEletronicoExistenteException(string message, Exception innerException) : base(message, innerException) { }
+}
---
+++ Negocio/Utilizadores/UtilizadoresService.cs
@@ -0,0 +1,63 @@
+using System.Net.Mail;
+
+using LI4.Dados;
+
+namespace LI4.Negocio.Utilizadores;
+
+public class UtilizadoresService
+{
+    public async Task<Utilizador.Tipo> IniciarSessao(string enderecoEletronico, string palavraPasse)
+    {
+        UtilizadorModel? utilizadorModel = await UtilizadoresRepository.Instance.Get(enderecoEletronico);
+        if (utilizadorModel == null)
+        {
+            throw new UtilizadorNaoEncontradoException();
+        }
+
+        Utilizador? utilizador = Utilizador.DeModel(utilizadorModel);
+
+        byte[] hash = Utilizador.HashDaPalavraPasse(palavraPasse);
+        if (!Enumerable.SequenceEqual(hash, utilizador.PalavraPasse))
+        {
+            throw new PalavraPasseIncorretaException();
+        }
+
+        if (!utilizador.PossivelIniciarSessao)
+        {
+            throw new ImpedidoDeIniciarSessaoException();
+        }
+
+        return utilizador.TipoDeConta;
+    }
+
+    public async Task RegistarUtilizador(string enderecoEletronico, string nomeCivil, string palavraPasse, Utilizador.Tipo tipoDeConta)
+    {
+        try
+        {
+            new MailAddress(enderecoEletronico);
+        }
+        catch (FormatException)
+        {
+            throw new EnderecoEletronicoInvalidoException();
+        }
+
+        UtilizadorModel? existente = await UtilizadoresRepository.Instance.Get(enderecoEletronico);
+        if (existente != null)
+        {
+            throw new EnderecoEletronicoExistenteException();
+        }
+
+        byte[] hash = Utilizador.HashDaPalavraPasse(palavraPasse);
+        String tipoString = Utilizador.StringDeTipo(tipoDeConta);
+        UtilizadorModel model = new UtilizadorModel
+        {
+            EnderecoEletronico = enderecoEletronico,
+            NomeCivil = nomeCivil,
+            PalavraPasse = hash,
+            TipoDeConta = tipoString,
+            PossivelIniciarSessao = true
+        };
+
+        await UtilizadoresRepository.Instance.Add(model);
+    }
+}
